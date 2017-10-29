08817bc653f2528b69ed7c6e8aee0ddcccd19aae
KOM2005 <oleh_kmyta@comcast.net>
2017-10-29 15:06:34 -0600

    completed

--- db/schema.sql
+++ db/schema.sql
@@ -5,5 +5,5 @@ CREATE TABLE burgers(
 id int AUTO_INCREMENT,
 PRIMARY KEY (id),
 burger_name varchar(50) NOT NULL,
-devoured BOOLEAN default false
-);
+devoured BOOLEAN default false,
+date TIMESTAMP);
